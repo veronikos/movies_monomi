7baefe51d7961c2094a5bd56dbb7ca8b31cc1702
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-06-16 14:51:30 +0300

    add nane of the film on hover

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,27 +1,40 @@
-import axios from 'axios';
+import axios from "axios";
 
-import React from 'react';
+import React from "react";
 
 export class Content extends React.Component {
   state = {
-    movies: []
-  }
+    movies: [],
+  };
 
   componentDidMount() {
-    axios.get(`https://wookie.codesubmit.io/movies`, { headers: {"Authorization" : `Bearer Wookie2021`} })
-      .then(res => {
+    axios
+      .get(`https://wookie.codesubmit.io/movies`, {
+        headers: { Authorization: `Bearer Wookie2021` },
+      })
+      .then((res) => {
         const movies = res.data.movies;
         this.setState({ movies });
-        console.log(movies[0])
-      })
+        console.log(movies[0]);
+      });
   }
 
   render() {
     return (
-        <div>
-            {/* {this.state.movies.map(movie => <li>{movie.title}</li>)} */}
-        {this.state.movies.map(movie => <img className="photoMovie" src={movie.backdrop} alt={movie.title}/>)}
+      <div>
+        {/* {this.state.movies.map(movie => <li>{movie.title}</li>)} */}
+        {this.state.movies.map((movie) => (
+          <div class="img__wrap">
+                      <img
+              className="photoMovie"
+              src={movie.backdrop}
+              alt={movie.title}
+            /><div class="img__description_layer">
+            <p class="img__description">{movie.title}</p>
+          </div>
         </div>
-    )
+        ))}
+      </div>
+    );
   }
 }
