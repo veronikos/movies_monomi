8d5fb3b03eaea620d69da02981f99a790bcf0faa
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-06-17 22:48:59 +0300

    add link to homepage from logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
   return (
     <div className="Header">
       <div className="blueLine">
-          <div className="square"></div>
-          <div className="square yellowSquare"></div>
-          <div className="square greenSquare"></div>
+        <div className="square"></div>
+        <div className="square yellowSquare"></div>
+        <div className="square greenSquare"></div>
       </div>
       <header className="App-header">
         <div className="logo">
-          <div>WOOKIE</div>
-          <div>MOVIES</div>
+          <Link to="/" className="logo_homepage_link">
+            <div>WOOKIE</div>
+            <div>MOVIES</div>
+          </Link>
         </div>
         <div className="inputHolder">
           <input></input>
